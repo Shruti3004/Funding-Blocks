8d4c249fc3e8f9b2ed7092d869d366d2c327dd81
Shruti3004 <mail2shruti.ag@gmail.com>
2021-08-25 15:11:06 +0530

    feat(footer): added the footer
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <>
+      <div className="bg-tertiaryColor text-white text-center py-4 font-18">
+        <span className="font-bold font-22">Funding Blocks</span> is offering an
+        opportunity for community members and investors to help.
+      </div>
+      <div className="footer-bg bg-primaryColor py-5 text-white">
+        <div className="flex-container container pt-4 pb-5 font-medium">
+          <div className="px-4">Name</div>
+          <div className="px-4">Address</div>
+          <div className="px-4">Edit Profile</div>
+          <div className="px-4">Current Balance</div>
+          <div className="px-4">Register</div>
+          <div className="px-4">Login</div>
+        </div>
+        <hr />
+        <div className="text-center pt-3">
+          {new Date().getFullYear()} &copy; Team Funding Blocks
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <>Navbar</>;
+  return <></>;
 };
 
 export default Navbar;
