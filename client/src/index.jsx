f302bca00aa12280ed4de8e4d358a7bc6f0852c4
Shruti3004 <mail2shruti.ag@gmail.com>
2021-08-30 16:32:29 +0530

    added the pwa files
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -14,9 +13,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-serviceWorker.register();
