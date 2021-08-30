c549cfbf43d836dce187d8acef52b37146274a30
Shruti3004 <mail2shruti.ag@gmail.com>
2021-08-30 05:44:34 +0530

    feat(home): added the funds section
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -12,7 +12,6 @@ const Navbar = () => {
   }, []);
 
   const logout = async () => {
-
     setIsLoggedIn(false);
   };
 
@@ -27,6 +26,9 @@ const Navbar = () => {
           <NavCustom.Collapse id="responsive-navbar-nav" className="text-white">
             <Nav className="me-auto"></Nav>
             <Nav>
+              <Nav.Link as={Link} to="/" className="text-white font-demi px-4">
+                Home
+              </Nav.Link>
               <Nav.Link as={Link} to="/about" className="text-white font-demi px-4">
                 About
               </Nav.Link>
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -44,6 +44,101 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className="custom-blue-bg py-section">
+        <h1 className="text-white text-center px-4">Causes you can raise funds for</h1>
+        <p className="text-white font-18 font-regular px-4 text-center mb-4">
+          Be it for a personal need, social cause or a creative idea - you can count on us for the
+          project that you want to raise funds for.
+        </p>
+        <div className="container px-4">
+          <div className="row d-flex justify-content-center">
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/education.jpeg" alt="education" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/cancer.jpeg" alt="cancer" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/animals.jpeg" alt="animals" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/heart.jpeg" alt="heart" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/child-health.jpeg" alt="child-health" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/accident.jpeg" alt="accident" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/brain-injury.jpeg" alt="brain-injury" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/tsunami.jpeg" alt="tsunami" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/wildlife-fire.jpeg" alt="wildlife-fire" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/covid.jpeg" alt="covid" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/education.jpeg" alt="education" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-6 col-6 mt-4">
+              <div className="card">
+                <div className="card-img-top">
+                  <img src="/images/education.jpeg" alt="education" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
