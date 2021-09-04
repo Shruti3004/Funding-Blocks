876a7ec652e8a84bdcde5c2cdc787dd1a2c2d623
Shruti3004 <mail2shruti.ag@gmail.com>
2021-09-04 11:12:32 +0530

    added the default page
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- client/src/pages/Default/index.jsx
+++ client/src/pages/Default/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import error from "../../assets/images/error.svg";
 
 const Default = () => {
-  return <></>;
+  return (
+    <div className="text-center container">
+      <div className="row d-flex justify-content-center">
+        <div className="col-lg-6 col-md-8 col-sm-12 col-12">
+          <img src={error} alt="page not found" className="img-fluid my-5" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Default;
